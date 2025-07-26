f7f4d17e02745ee392890eaeb50f0a1b4cf0ad32
RyadLemmouDevWeb <ryad.lemmou.94@gmail.com>
2025-07-26 22:50:05 +0200

    [FIX] : problem with the carousel

--- @/src/component/partial/projet.jsx
+++ @/src/component/partial/projet.jsx
@@ -11,13 +11,13 @@ export default function Projet() {
       description: 'Dans le cadre d\'un projet de groupe, nous avons conçu et développé un clone fonctionnel de Twitter en utilisant PHP, MySQL, JavaScript et HTML. Cette plateforme de microblogging permet aux utilisateurs de s\'inscrire, se connecter, publier des tweets, liker des publications, et interagir avec une interface responsive. Ce projet nous a permis de collaborer efficacement pour gérer la structure backend avec MySQL, développer des fonctionnalités dynamiques en PHP et JavaScript, et concevoir une interface intuitive en HTML. Vous pouvez consulter le code source dans le lien GitHub ou explorer la maquette.',
       githubUrl: 'https://github.com/RyadLemmouDevWeb/1Twitter2Plus',
       images: [
-        'public/assets/Twitter1.png',
-        'public/assets/Twitter2.png',
-        'public/assets/Twitter3.png',
-        'public/assets/Twitter4.png',
-        'public/assets/Twitter5.png',
-        'public/assets/Twitter6.png',
-        'public/assets/Twitter7.png'
+        '/assets/Twitter1.png',
+        '/assets/Twitter2.png',
+        '/assets/Twitter3.png',
+        '/assets/Twitter4.png',
+        '/assets/Twitter5.png',
+        '/assets/Twitter6.png',
+        '/assets/Twitter7.png'
       ]
     },
     {
@@ -25,10 +25,10 @@ export default function Projet() {
       description: "Dans le cadre d'un projet solo, j'ai conçu et développé un site fonctionnel pour un cinéma en utilisant PHP, MySQL, JavaScript et HTML. Ce site permet aux utilisateurs de rechercher quels films ont été diffusés dans le cinéma avec des informations sur chacun, aussi il permet de rechercher un utilisateur en fonction de son nom, il permet aussi d'implémenter une nouvelle séance de cinéma dans la base de données. La page d'accueil utilise une API afin de mettre en avant les affiches de films. Vous pouvez consulter le code source dans le lien GitHub ou explorez la maquette.",
       githubUrl: 'https://github.com/RyadLemmouDevWeb/My_Cinema',
       images: [
-        "public/assets/Cinema1.png",
-        "public/assets/Cinema2.png",
-        "public/assets/Cinema3.png",
-        "public/assets/Cinema4.png"
+        "/assets/Cinema1.png",
+        "/assets/Cinema2.png",
+        "/assets/Cinema3.png",
+        "/assets/Cinema4.png"
       ]
     },
     {
@@ -36,10 +36,10 @@ export default function Projet() {
       description: 'J\'ai développé une version interactive du célèbre jeu de stratégie Puissance 4 en JavaScript. Ce projet, réalisé en solo, met en avant mes compétences en algorithmie et en gestion des interactions utilisateur. Le jeu permet à deux joueurs de s\'affronter sur une grille personnalisable, avec des fonctionnalités telles que la détection automatique de victoire et la possibilité de relancer une partie à tout moment. Ce projet illustre ma capacité à concevoir des solutions logiques tout en offrant une expérience utilisateur fluide et engageante.',
       githubUrl: 'https://github.com/RyadLemmouDevWeb/Puissance-4',
       images: [
-        'public/assets/Puissance4_1.png',
-        'public/assets/Puissance4_2.png',
-        'public/assets/Puissance4_3.png',
-        'public/assets/Puissance4_4.png'
+        '/assets/Puissance4_1.png',
+        '/assets/Puissance4_2.png',
+        '/assets/Puissance4_3.png',
+        '/assets/Puissance4_4.png'
       ]
     }
   ];
